09d534fd09ef0923d07479354f35026b48738ddc
SunidhiChoubey <sunidhichoubey721@gmail.com>
2024-07-23 15:07:37 +0530

    Add files via upload

---
+++ sunidhi2.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    float b;
+    char c;
+    short int d;
+    double e;
+    long int f;
+    signed short int g;
+    unsigned short int h;
+    signed long int i;
+    unsigned long int j;
+    long long int k;
+    unsigned long long int l; 
+
+    
+    cout<<"Size of int"<<sizeof(a)<<endl;
+    cout<<"Size of float"<<sizeof(b)<<endl;
+    cout<<"Size of char"<<sizeof(c)<<endl;
+    cout<<"Size of short int"<<sizeof(d)<<endl;
+    cout<<"Size of double"<<sizeof(e)<<endl;
+    cout<<"Size of long int"<<sizeof(f)<<endl;
+    cout<<"Size of  signed short int"<<sizeof(g)<<endl;
+    cout<<"Size of  unsigned short int"<<sizeof(h)<<endl;
+    cout<<"Size of  unsigned long int"<<sizeof(j)<<endl;
+    cout<<"Size of  signed long int"<<sizeof(i)<<endl;
+    cout<<"Size of  long long int"<<sizeof(k)<<endl;
+    cout<<"Size of  unsigned long long int"<<sizeof(l)<<endl;
+
+    
+
+    return 0;
+
+
+}
